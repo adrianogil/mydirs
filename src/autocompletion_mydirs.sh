53993f5658eb31ff680c7bcf851fef9a7031b3e6
Adriano Gil <adrianogil.san@gmail.com>
2016-07-31 17:28:31 -0400

    Added autocompletion script.
    Should be called on .profile as:
    
    source /path/to/mydirs/src/autocompletion_mydirs.sh

---
+++ src/autocompletion_mydirs.sh
@@ -0,0 +1,15 @@
+_mydirs() 
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="--save -s --open -o --remove -r --list -l"
+
+    
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+}
+complete -F _mydirs mydirs
