4a3e668ccdd36f51be9fa85be185a48b652c3ca9
Adriano Gil <adrianogil.san@gmail.com>
2016-11-07 09:21:05 -0400

    Escape string path

--- src/autocompletion_mydirs.sh
+++ src/autocompletion_mydirs.sh
@@ -1,11 +1,11 @@
-_mydirs() 
+_mydirs()
 {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     opts="--save -s --open -o --remove -r --list -l"
-    _script_folders=$(/Users/gil/workspace/python/mydirs/src/mydirs.py --auto-list)
+    _script_folders=$(~/workspace/python/mydirs/src/mydirs.py --auto-list)
 
 	if [[ "${prev}" == "--open" || "${prev}" == "-o" || "${prev}" == "--remove" || "${prev}" == "-r"  ]] ; then
 		COMPREPLY=( $(compgen -W "${_script_folders}" -- ${cur}) )
