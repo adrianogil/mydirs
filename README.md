406c005b9c7e6562fc9c271758e9c50f225a4357
Adriano Gil <adrianogil.san@gmail.com>
2017-09-18 20:12:55 -0400

    Added install script and improved README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # mydirs
 Command line tool to bookmark directories.
+
+# How to Install
+You only need to run
+
+```
+./install.sh
+```
+
+# Command line options
+
+Save current directory using <directory-alias>
+
+```
+mydirs -s <directory-alias>
+```
+
+Open directory from <directory-alias>
+```
+mydirs -o <directory-alias>
+```
---
+++ install.sh
@@ -0,0 +1,25 @@
+echo '================================'
+echo 'Installing mydirs'
+echo ''
+
+# Variables
+profile_file=~/.profile
+mydirs_directory=$(PWD)
+mydirs_script=$mydirs_directory/src/mydirs.sh
+mydirs_autocomplete_script=$mydirs_directory/src/autocompletion_mydirs.sh
+
+echo ' ' >>  $profile_file
+echo '# MyDirs ' >> $profile_file
+
+echo 'Creating alias at '$profile_file
+echo 'alias mydirs="'$mydirs_script'"' >> $profile_file
+
+echo 'Adding autocomplete feature'
+echo 'source "'$mydirs_autocomplete_script'"' >> $profile_file
+echo ''
+echo 'Update '$profile_file
+source $profile_file
+
+echo ' '
+echo 'Installation completed!'
+echo '================================'
