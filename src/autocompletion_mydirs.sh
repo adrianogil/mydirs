0a15f014fe4ebb013c0e78d49cad4f0c79e6f8e0
Adriano Gil <adrianogil.san@gmail.com>
2018-03-24 19:55:55 -0400

    Using python2 on autocompletion script

--- src/autocompletion_mydirs.sh
+++ src/autocompletion_mydirs.sh
@@ -5,7 +5,7 @@ _mydirs()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     opts="--save -s --open -o --remove -r --list -l --path -p"
-    _script_folders=$($MYDIRS_DIRECTORY/mydirs.py --auto-list)
+    _script_folders=$(python2 $MYDIRS_DIRECTORY/mydirs.py --auto-list)
 
 	if [[ "${prev}" == "--open" || "${prev}" == "-o" || "${prev}" == "--remove" || "${prev}" == "-r" || "${prev}" == "--path" || "${prev}" == "-p"  ]] ; then
 		COMPREPLY=( $(compgen -W "${_script_folders}" -- ${cur}) )
